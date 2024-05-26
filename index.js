60c8cbc00bb3148ff9de94700dcd529ff04d7ed7
Michaelson44 <149709538+Michaelson44@users.noreply.github.com>
2024-05-26 09:34:56 -0700

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
     const scrollPos = window.scrollY;
     console.log(scrollPos)
-
+    
     document.querySelector('.hero-back').style.backgroundSize = 100 + scrollPos + '%';
 })
